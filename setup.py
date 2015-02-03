bfbe53282b069a8f1f879b6e41abf1b77bb63aa0
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-02-03 14:44:51 +0000

    square brackets causing namespace error?

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='dmt.manifestcompiler',
     version='1.0',
-    namespace_packages=['dmt'],
+    namespace_packages='dmt',
     packages=['manifestcompiler',],
 )
