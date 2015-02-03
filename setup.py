95937d04edfd16abaf949b4353363cb2ab9ce6df
calvin butcher <calvin.butcher@bodleian.ox.ac.uk>
2015-02-03 12:53:38 +0000

    added init.py to dmt/

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
-setup(name='manifestcompiler',
-version='1.0',
-packages=['manifestcompiler',],
+setup(
+    name='dmt.manifestcompiler',
+    version='1.0',
+    namespace_packages=['dmt'],
+    packages=['manifestcompiler',],
 )
